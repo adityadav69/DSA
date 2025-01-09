3489cfefd1473275ecd47404aa1a50c21a8435cf
Aditya yadav <adityayadaw42@gmail.com>
2025-01-09 22:17:05 +0530

    pairs

---
+++ Array/Pairs_in_Array.java
@@ -0,0 +1,15 @@
+public class Pairs_in_Array {
+
+    public static void printPairs(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                System.out.print("("+arr[i]+","+arr[j]+")");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={2,3,5,8};
+        printPairs(arr);
+    }
+}
