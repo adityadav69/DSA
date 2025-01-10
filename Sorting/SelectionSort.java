b683f43f8a49ac7f7fbbecfe9546f82d73dda72d
Aditya yadav <adityayadaw42@gmail.com>
2025-01-10 15:25:54 +0530

    selection sort

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,26 @@
+public class SelectionSort {
+
+    public static void sort(int arr[]){
+
+        for(int i=0;i<arr.length-1;i++){
+            int minpos=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[minpos]>arr[j]){
+                    int temp=arr[minpos];
+                    arr[minpos]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
+    public static void print(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={3,4,63,1,2,1};
+        sort(arr);
+        print(arr);
+    }
+}
