485cdb3ceeb0e6ee002e9d700e706c64f93754c1
Aditya yadav <adityayadaw42@gmail.com>
2025-01-13 23:31:54 +0530

    practice

--- practice/bubble.java
+++ practice/bubble.java
@@ -1,12 +1,13 @@
-public class BubbleSort{
+public class bubble{
 
     public static void sort(int arr[]){
         for(int i=0;i<arr.length-1;i++){
-            for(int j=0;j<arr.length-1-i;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+            for(int j=1;j<arr.length-i;j++){
+
+                if(arr[i]>arr[i+1]){
+                    int temp=arr[i];
+                    arr[i]=arr[i+1];
+                    arr[i+1]=temp;
                 }
             }
         }
@@ -16,11 +17,9 @@ public class BubbleSort{
             System.out.println(arr[i]);
         }
     }
-
     public static void main(String[] args) {
-        int arr[]={3,4,1};
+        int arr[]={1,7,3,4,8};
         sort(arr);
         print(arr);
-
     }
 }
