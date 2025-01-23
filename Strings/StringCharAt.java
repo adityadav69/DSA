3bfc4d3ad2c6111b8a5663aee2b6ccd0ef2b2466
Aditya yadav <adityayadaw42@gmail.com>
2025-01-23 18:12:21 +0530

    DSA

---
+++ Strings/StringCharAt.java
@@ -0,0 +1,10 @@
+public class StringCharAt {
+    public static void main(String[] args) {
+        String s="Aditya";
+        System.out.println(s.charAt(5));
+        System.out.println();
+        for(int i=0;i<s.length();i++){
+            System.out.print(s.charAt(i));
+        }
+    }
+}
