f0b3313179abc4ece9769b3143c78d3801ab10e5
Aditya yadav <adityayadaw42@gmail.com>
2025-01-23 18:10:03 +0530

    DSA

---
+++ Strings/StringConcate.java
@@ -0,0 +1,8 @@
+public class StringConcate {
+    public static void main(String[] args) {
+        String first_name="Aditya";
+        String last_name="Yadav";
+        String Full_name=first_name+" "+last_name;
+        System.out.println(Full_name);
+    }
+}
