c8212109216d0d59e7f4fa35adada32b31738319
Aditya yadav <adityayadaw42@gmail.com>
2025-01-23 19:01:00 +0530

    DSA

---
+++ Strings/LargestString.java
@@ -0,0 +1,16 @@
+public class LargestString {
+
+    public static void largestString(String fruits[]){
+        String largest=fruits[0];
+        for(int i=1;i<fruits.length;i++){
+            if(fruits[i].compareTo(largest)>0){
+                largest=fruits[i];
+            }
+        }
+        System.out.println(largest);
+    }
+    public static void main(String[] args) {
+        String fruits[]={"Mango","Bannana","Apple"};
+        largestString(fruits);
+    }
+}
