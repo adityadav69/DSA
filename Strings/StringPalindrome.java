561c9bd9232c250b28d721a286426c8a3741c9f4
Aditya yadav <adityayadaw42@gmail.com>
2025-01-23 18:16:29 +0530

    DSA

---
+++ Strings/StringPalindrome.java
@@ -0,0 +1,20 @@
+public class StringPalindrome {
+
+    public static boolean checkPalindrome(String s){
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i)!=s.charAt(s.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s="noon";
+        if(checkPalindrome(s)==true){
+            System.out.println("String are palindrome");
+        }
+        else{
+            System.out.println("Not palindrome");
+        }
+    }
+}
