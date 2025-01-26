c314addddad2e2f31727860a6e2e31af85b06d8b
Aditya yadav <adityayadaw42@gmail.com>
2025-01-26 22:49:08 +0530

    DSA

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -18,7 +18,7 @@ public class BubbleSort{
     }
 
     public static void main(String[] args) {
-        int arr[]={3,4,1};
+        int arr[]={4,56,3,53,2,1};
         sort(arr);
         print(arr);
 
