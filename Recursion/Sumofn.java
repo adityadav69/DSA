38a1005581c71f53c2da69c3a13eab9677bb4272
Aditya yadav <adityayadaw42@gmail.com>
2025-01-27 20:22:39 +0530

    DSA

---
+++ Recursion/Sumofn.java
@@ -0,0 +1,15 @@
+public class Sumofn {
+
+    public static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int snm1 = sum(n - 1);
+        int sum = n + snm1;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+}
--- Recursion/factorial.java
+++ Recursion/factorial.java
@@ -4,9 +4,11 @@ public class factorial {
         if(n==0){
             return 1;
         }
+
         int fnm1=Calcfactorial(n-1);
         int fact=n*fnm1;
         return fact;
+        
     }
     public static void main(String[] args) {
         System.out.println(Calcfactorial(5));
