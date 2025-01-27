c11710a11e07918f95b1785209ca514e25e0e3a1
Aditya yadav <adityayadaw42@gmail.com>
2025-01-27 21:26:26 +0530

    DSA

---
+++ Recursion/checkSortedArray.java
@@ -0,0 +1,16 @@
+public class checkSortedArray {
+
+    public static boolean check(int arr[],int i){
+        if(i==arr.length-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+        return check(arr, i+1);
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,7,2};
+        System.out.println(check(arr, 0));
+    }
+}
