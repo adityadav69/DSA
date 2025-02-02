5a7cd9fb1a04d51b4cb1bd8edf109f303c48a846
Aditya yadav <adityayadaw42@gmail.com>
2025-02-02 19:06:26 +0530

    DSA

---
+++ Oops/AcccessModifier.java
@@ -0,0 +1,29 @@
+public class AcccessModifier {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.name="Aditya";
+        s1.roll_no=13;
+        System.out.println(s1.name);
+        System.out.println(s1.roll_no);
+        // s1.pass="afd";//This will throw error because pass is private variable
+    }
+}
+
+class Student{
+    String name;
+    int roll_no;
+    private String pass;
+}
+
+/*
+AcccessModifier     withinClass     withinPackage       outsidePackageBySubClass        outside package
+
+private                 yes             no                  no                                  no
+
+Default                 yes             yes                 no                                  no
+
+protected               yes             yes                 yes                                 no
+
+public                  yes             yes                 yes                                 yes
+
+*/
--- Oops/Basicoops.java
+++ Oops/Basicoops.java
@@ -1,13 +1,18 @@
-public class Basicoops{
+public class basicoops{
+    // class Student{
 
+    // }// When we intitalize class here then we can't create object of this class from main function because main functioon is static function if we want then we have to make this class as static
     public static void main(String[] args) {
-        Animal horse=new Animal();
-        horse.walk();
+        
+        Student s1=new Student();
+        s1.name="Aditya";
+        s1.roll_no=13;
+        System.out.println(s1.name);
+        System.out.println(s1.roll_no);
+
     }
 }
-class Animal{
-    int feet;
-    void walk(){
-        System.out.println("Walking..");
-    }
+class Student{
+    String name;
+    int roll_no;
 }
