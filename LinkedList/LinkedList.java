5f8df9b15c52c5b124b687863f065ec55f5ae25a
Aditya yadav <adityayadaw42@gmail.com>
2025-02-08 18:49:42 +0530

    DSA

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,122 +1,137 @@
 public class LinkedList {
 
-    public static class Node{
+    public static class Node {
         int data;
         Node next;
-        Node(int data){
-            this.data=data;
-            this.next=null;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
+
     public static Node head;
     public static Node tail;
 
-    static int size=0;
+    static int size = 0;
 
-    public void addFirst(int data){
-        Node newNode=new Node(data);
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
 
-        if(head==null){
-            head=tail=newNode;
+        if (head == null) {
+            head = tail = newNode;
             size++;
             return;
         }
 
-        newNode.next=head;
-        head=newNode;
+        newNode.next = head;
+        head = newNode;
         size++;
     }
-    public void addLast(int data){
-        Node newNode=new Node(data);
-        if(head==null){
-            head=tail=newNode;
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
             size++;
             return;
         }
 
-       tail.next=newNode;
-       tail=newNode;
-       size++;
+        tail.next = newNode;
+        tail = newNode;
+        size++;
     }
 
-    public void addIndex(int index,int data){
-        if(index>size || index<0){
+    public void addIndex(int index, int data) {
+        if (index > size || index < 0) {
             System.out.println("Please enter a index in the size of ll");
             return;
         }
-        if(index==0){
+        if (index == 0) {
             addFirst(data);
             return;
         }
-        Node temp=head;
-        int tempIndex=0;
-        while (tempIndex<index-1) {
-            temp=temp.next;
+        Node temp = head;
+        int tempIndex = 0;
+        while (tempIndex < index - 1) {
+            temp = temp.next;
             tempIndex++;
         }
-        Node newNode=new Node(data);
-        newNode.next=temp.next;
-        temp.next=newNode;
+        Node newNode = new Node(data);
+        newNode.next = temp.next;
+        temp.next = newNode;
         size++;
     }
 
-    public int removeFirst(){
-        if(size==0){
+    public int removeFirst() {
+        if (size == 0) {
             System.out.println("There is no Element in the ll to remove");
             return Integer.MIN_VALUE;
-        }
-        else if(size==1){
-            int val=head.data;
-            head=head.next;
-            size=0;
+        } else if (size == 1) {
+            int val = head.data;
+            head = head.next;
+            size = 0;
             return val;
         }
-        int val=head.data;
-        head=head.next;
+        int val = head.data;
+        head = head.next;
         size--;
         return val;
     }
-    public int removeLast(){
-        if(size==0){
+
+    public int removeLast() {
+        if (size == 0) {
             System.out.println("There is no Element in the ll to remove");
             return Integer.MIN_VALUE;
-        }
-        else if(size==1){
-            int val=head.data;
-            head=head.next;
-            size=0;
+        } else if (size == 1) {
+            int val = head.data;
+            head = head.next;
+            size = 0;
             return val;
         }
-        int i=0;
-        Node temp=head;
-        while (i<size-1) {
-            temp=temp.next;
+        int i = 0;
+        Node temp = head;
+        while (i < size - 1) {
+            temp = temp.next;
             i++;
         }
 
-        int val=tail.data;
-        temp.next=null;
-        tail=temp;
+        int val = tail.data;
+        temp.next = null;
+        tail = temp;
         size--;
         return val;
-    
+
     }
 
-    public void print(){
-        if(head==null){
+    public int search(int key) {
+        Node temp = head;
+        int index = 0;
+        while (temp != null) {
+            if (temp.data == key) {
+                return index;
+            }
+            index++;
+            temp = temp.next;
+        }
+        return -1;
+    }
+
+    public void print() {
+        if (head == null) {
             System.out.println("Linked List is Empty");
             return;
         }
-        Node temp=head;
-        while (temp!=null) {
-            System.out.print(temp.data+"->");
-            temp=temp.next;
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
         }
         System.out.println("null");
     }
 
     public static void main(String[] args) {
-        LinkedList ll=new LinkedList();
+        LinkedList ll = new LinkedList();
         ll.print();
         ll.addFirst(1);
         ll.print();
@@ -138,6 +153,7 @@ public class LinkedList {
         System.out.println(ll.removeFirst());
         ll.print();
         System.out.println(size);
+        System.out.println(ll.search(999));
 
     }
 }
