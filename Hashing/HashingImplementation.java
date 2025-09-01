c8b2f1a141d67d24c97b37248d3acfe4ceae5304
Aditya yadav <adityayadaw42@gmail.com>
2025-09-01 22:06:41 +0530

    HashFunction

--- Hashing/HashingImplementation.java
+++ Hashing/HashingImplementation.java
@@ -5,13 +5,27 @@ public class HashingImplementation {
         //Insertion in hashmap
         hm.put("india", 150);
         hm.put("Nepal", 4);
-        hm.put("Indonesia", 12);
+        hm.put("China", 12);
 
         //Get in hashmap
         // System.out.println(hm);
         System.out.println(hm.get("india"));
 
         //Containsin
-        System.out.println(hm.containsKey("indonesia"));
+        System.out.println(hm.containsKey("Indonesia"));
+
+
+        System.out.println(hm);
+        //remove function
+        System.out.println(hm.remove("China"));
+        System.out.println(hm);
+
+        //getSize;
+        System.out.println(hm.size());
+
+        //clear
+        hm.clear();
+        System.out.println(hm);
+
     }
 }
