8c8d5c50b6a1faa05f59a52172d3e3d6c43294db
Aditya yadav <adityayadaw42@gmail.com>
2025-10-22 20:34:05 +0530

    tree

--- Tree/practice/PreOrder.c
+++ Tree/practice/PreOrder.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 struct node{
     struct node* left;
     struct node* right;
@@ -13,19 +13,56 @@ struct node * createNode(int data){
     n->left=NULL;
     n->right=NULL;
     n->data=data;
+    return n;
+}
+
+void preorder(struct node *root){
+    if(root==NULL){
+        return;
+    }
+
+    printf("%d",root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(struct node * root){
+    if(root==NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d",root->data);
+}
+
+void inorder(struct node * root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    printf("%d",root->data);
+    inorder(root->right);
 }
 
 int main(){
 
-    struct node * p=createNode(4);
-    struct node * p1=createNode(1);
-    struct node * p2=createNode(6);
-    struct node * p3=createNode(5);
-    struct node * p4=createNode(2);
+    struct node * p=createNode(7);
+    struct node * p1=createNode(2);
+    struct node * p2=createNode(1);
+    struct node * p3=createNode(0);
+    struct node * p4=createNode(4);
+
     p->left=p1;
     p->right=p2;
     p1->left=p3;
     p1->right=p4;
 
+
+    preorder(p);
+    printf("\n");
+    postorder(p);
+    printf("\n");
+    inorder(p);
     return 0;
+
 }
