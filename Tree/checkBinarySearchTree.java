5c28ef19abba753c0497a282087ecebf6f169e5c
Aditya yadav <adityayadaw42@gmail.com>
2025-10-26 12:46:29 +0530

    check bst by extra array

---
+++ Tree/checkBinarySearchTree.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class checkBinarySearchTree {
+     static class Node {
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    static ArrayList<Integer> helper(Node root,ArrayList<Integer>ans){
+        if(root==null){
+            return ans;
+        }
+        helper(root.left, ans);
+        ans.add(root.data);
+        helper(root.right, ans);
+        return ans;
+    }
+
+    static boolean checkBinarySearchTrees(Node root){
+        ArrayList<Integer> list=helper(root, new ArrayList<>());
+        for(int i=1;i<list.size();i++){
+            if(list.get(i)<list.get(i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root=new Node(6);
+        Node p2=new Node(3);
+        Node p3=new Node(2);
+        Node p4=new Node(9);
+        Node p5=new Node(3);
+        root.left=p2;
+        root.right=p5;
+        p2.left=p3;
+        p2.right=p4;
+        inorder(root);
+        System.out.println(checkBinarySearchTrees(root));
+    }
+}
